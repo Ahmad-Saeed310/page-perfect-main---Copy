b35d2c97a020ba936d304547036e7fb07478f25c
ahmad-saeed310 <f23bdocs1e02215@iub.edu.pk>
2026-02-19 13:48:00 +0500

    adchange

--- src/components/ads/ad.jsx
+++ src/components/ads/ad.jsx
@@ -10,21 +10,14 @@ const GoogleAd = () => {
   }, []);
 
   return (
-  <ins
-  className="adsbygoogle"
-  style={{
-    display: "block",
-    width: "100%",
-    minHeight: "250px",
-    backgroundColor: "#f3f3f3" // optional: temporary background for testing
-  }}
-  data-ad-client="ca-pub-1165028160233098"
-  data-ad-slot="7760276067"
-  data-ad-format="auto"
-  data-full-width-responsive="true"
-  data-adtest="on" // test mode
-/>
-
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-1165028160233098"
+      data-ad-slot="7760276067"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 };
 
